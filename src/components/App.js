0700490a9bf53a84ff92aeb5c351bc1495b07969
grantspell <spellgrant@gmail.com>
2017-10-10 18:07:29 -0400

    FINAL | copied Dannys given solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import RandomGif from './RandomGif';
-import SavedGifsList from "./SavedGifsList";
 
+import SavedGifs from './SavedGifs.jsx'
+import RandomGif from './RandomGif.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img className="backgroundImg" src="http://i.imgur.com/DysM91b.png"/>
-        <h1>Hello World</h1>
+        <img className="backgroundImg" src="http://i.imgur.com/DysM91b.png" />
+        <h1>Strange Things</h1>
+        <RandomGif />
+        <SavedGifs />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
